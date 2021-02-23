235389ffb63e1f2885247306a53debbc3a1eaa12
John <jddhalogamer@gmail.com>
2021-02-22 18:38:26 -0600

    made mock db in json file, and added code to fetch page

---
+++ BlazorRestaurantApp/Data/menu-items.jsx
@@ -0,0 +1,17 @@
+﻿[
+    {
+        "Entree" : "steak",
+        "Price": "$" + 20,
+        "Side" : "fries"
+    },
+    {
+        "Entree" : "spaghetti",
+        "Price": "$" + 15,
+        "Side" : "toast"
+    },
+    {
+        "Entree": "soup",
+        "Price": "$" + 12,
+        "Side" : "salad"
+    }
+]
